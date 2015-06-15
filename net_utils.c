0b3639b4c32f4a1e94b9fd84b7e0821889a0acfa
KoynovStas <skojnov@yandex.ru>
2015-06-15 11:54:40 +0300

    add net_utils

---
+++ net_utils.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>  
+#include <unistd.h>  
+#include <fcntl.h>
+
+
+#include "net_utils.h"
---
+++ net_utils.h
@@ -0,0 +1,8 @@
+#ifndef NET_UTILS_HEADER
+#define NET_UTILS_HEADER
+
+
+
+
+
+#endif //NET_UTILS_HEADER
